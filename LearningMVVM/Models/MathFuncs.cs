807a9c3bbc138b84f879dcb0314cc5fd3272ee99
Natalya <akva888@mail.ru>
2022-10-07 09:36:46 +0300

    Snd example with commands

--- LearningMVVM/Models/MathFuncs.cs
+++ LearningMVVM/Models/MathFuncs.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LearningMVVM
+namespace LearningMVVM.Models
 {
     static class MathFuncs
     {
---
+++ LearningMVVM/ViewModels/ListVM.cs
@@ -0,0 +1,36 @@
+﻿using LearningMVVM.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningMVVM.ViewModels
+{
+    public class ListVM : BindableBase
+    {
+        readonly ListModel _model = new ListModel();
+        public ListVM()
+        {
+            // sending model changed props to view
+            _model.PropertyChanged += (s, e) => { RaisePropertyChanged(e.PropertyName); };
+            AddCommand = new DelegateCommand<string>(str =>
+            {
+                // input valid check
+                int ival;
+                if (int.TryParse(str, out ival)) _model.AddValue(ival);
+            });
+            RemoveCommand = new DelegateCommand<int?>(i => {
+                if (i.HasValue) _model.RemoveValue(i.Value);
+            });
+        }
+
+        public DelegateCommand<string> AddCommand { get; }
+        public DelegateCommand<int?> RemoveCommand { get; }
+        public int Sum => _model.Sum;
+        public ReadOnlyObservableCollection<int> ListPublicValues => _model.ListPublicValues;
+    }
+}
